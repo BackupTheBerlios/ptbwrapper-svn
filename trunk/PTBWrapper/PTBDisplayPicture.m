4e53847753a5be321dd25e8f3f678ae8ba1a428b
dkbemis <dkbemis@2733e20e-e661-364c-b241-bfbd7ef7a67c>
2010-03-13 05:25:39 +0000

    Initial trigger delays

--- trunk/PTBWrapper/PTBDisplayPicture.m
+++ trunk/PTBWrapper/PTBDisplayPicture.m
@@ -6,6 +6,7 @@
 % Args:
 %	- picture: The picture file to display
 %	- duration: The length to display for.
+%   - tag: A label to print out with the picture.
 %	- trigger: A trigger to send (optional)
 %
 % Usage: PTBDisplayPicture('Test.jpg',{.3})
@@ -16,7 +17,17 @@
 
 % TODO: Take variable args and parse.
 % TODO: Error checking.
-function PTBDisplayPicture(picture, duration, varargin)
+function PTBDisplayPicture(picture, position, duration, tag, varargin)
+
+% TODO: Figure out best way to pass through varargin.
+% Right now, it becomes wrapped in too many {}s
+if length(varargin) > 1
+    error('Too many varargs for now. Exiting...');
+end
+arg = '';
+if ~isempty(varargin) > 0
+    arg = varargin{1};
+end
 
 % TODO: Allow setting of size, orientation, position, etc.
 
@@ -28,5 +39,5 @@ imdata = {};
 imdata{1} = imread(picture);
 
 % Lean on the matrices routine
-PTBDisplayMatrices(imdata, {'center'}, duration, varargin);
+PTBDisplayMatrices(imdata, {'center'}, duration, tag, arg);
 
--- trunk/PTBWrapper/PTBSendTrigger.m
+++ trunk/PTBWrapper/PTBSendTrigger.m
@@ -21,13 +21,25 @@ if ~PTBUSBBoxInitialized
 	return;
 end
 
+% Trigger seems to get there too fast.
+% Despite the fact that the flip and trigger times are
+% the same
+trigger_delay = 0.016;
+
 % Send the trigger
 global PTBUSBBoxDeviceID;
 global PTBTriggerLength;
+pause(trigger_delay);
+trig_time = GetSecs;
 PsychHID('SetReport', PTBUSBBoxDeviceID, 2, hex2dec('32'), uint8(zeros(1,2)+value));
 pause(PTBTriggerLength);
 PsychHID('SetReport', PTBUSBBoxDeviceID, 2, hex2dec('32'), uint8(zeros(1,2)));
 
+trig_time_2 = GetSecs;
+
+
 % Want to record
-global PTBLogAppend;
-PTBLogAppend{end+1} = num2str(value);
+global PTBLogFileID;
+PTBWriteLog(PTBLogFileID, 'TRIGGER', 'USBBox', num2str(value), trig_time);	
+
+    
