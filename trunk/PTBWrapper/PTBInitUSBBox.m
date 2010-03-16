7bf4fdce68a94cdcf75cf9ae4b7d4a44eb211384
dkbemis <dkbemis@2733e20e-e661-364c-b241-bfbd7ef7a67c>
2010-03-16 05:07:57 +0000

    Made PTBInitUSBBox not crash on failure

--- trunk/PTBWrapper/PTBInitUSBBox.m
+++ trunk/PTBWrapper/PTBInitUSBBox.m
@@ -11,18 +11,24 @@
 
 function PTBInitUSBBox
 
+global PTBUSBBoxInitialized;
+global PTBUSBBoxDeviceID;
+
 % Attempt to find the device number for the
 % IOLab USB Box
 devices = squeeze(struct2cell(PsychHID('devices')));
 devices_id = cell2mat(devices(6,:));
 results = find(devices_id == 6588);
 if isempty(results)
-    error('BBox not found. Exiting...');
+	PTBUSBBoxDeviceID = -1;
+	PTBUSBBoxInitialized = 0;
+    disp('WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!');
+	disp('BBox not found. No triggers will be sent.');
+    disp('WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!');
+	return;
 end;
 
 % Set all the triggers to 0
-global PTBUSBBoxDeviceID;
-global PTBUSBBoxInitialized;
 PTBUSBBoxDeviceID = results(1);
 PsychHID('SetReport', PTBUSBBoxDeviceID, 2, hex2dec('32'), uint8([0 0]));
 disp('BBox found and ready to go.');
