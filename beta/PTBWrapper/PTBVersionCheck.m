c638bfba68c9e37e1522a2f0b2d7857916774c2c
dkbemis <dkbemis@2733e20e-e661-364c-b241-bfbd7ef7a67c>
2010-03-17 00:55:49 +0000

    New changes

--- beta/PTBWrapper/PTBVersionCheck.m
+++ beta/PTBWrapper/PTBVersionCheck.m
@@ -21,20 +21,31 @@
 % Date: 3/1/10
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function PTBVersionCheck(major, minor, point, mode)
+function PTBVersionCheck(major, minor, point, mode, info, recursive)
 
 % Grab the info
-info = PTBWrapperVersion;
+if nargin < 5
+    info = [];
+end
+if isempty(info)
+    info = PTBWrapperVersion;
+end
+
+% Should we recurse or not
+if nargin < 6
+	recursive = 1;
+end
+
 
 % Check and error
 for i = 1:3
-	if checkValue(i, mode, info, [major, minor, point])
+	if checkValue(i, mode, info, [major, minor, point], recursive)
 		return;
 	end	
 end
 
 % Helper...
-function value = checkValue(curr, mode, info, version)
+function value = checkValue(curr, mode, info, version, recursive)
 
 % Set what we're checking
 version_parts = {'major','minor','point'};
@@ -79,6 +90,11 @@ end
 % Might have errored
 if value < 0	
 	
+	% Might just be recursing
+	if ~recursive
+		error('Bad version');
+	end
+	
 	% Show that we errored
 	disp(' ');
 	disp(['Version is no good. Need ' mode ' ' num2str(version(1)) '.' ...
@@ -87,24 +103,36 @@ if value < 0
 	disp(' ');
 	
 	% Give a (hopefully) useful message
-	
-	if strcmpi(mode, 'at least') || strcmpi(mode, 'more than')
-		disp('Try running the command UpdatePTBWrapper.');
-	elseif strcmpi(mode, 'exactly') || strcmpi(mode, 'no more than')
-		disp(['Try running the command UpdatePTBWrapper([], ' num2str(version(1)) '.' ...
-		num2str(version(2)) '.' num2str(version(3)) ').']);
-	else
-		disp('Try running the command UpdatePTBWrapper([], VERSION).');
-		disp(' ');
-		d_str = 'Where VERSION is one of the following: ';
-		for i = 1:length(info.past_versions)
-			d_str = [d_str '; ' info.past_versions{i}];
+	for i = 1:length(info.past_versions)
+		
+		 % Make the new version info to check against
+		new_v = info.past_versions{i}{1};
+		[new_info.major new_v] = strtok(new_v,'.');
+		[new_info.minor new_v] = strtok(new_v,'.');
+		[new_info.point new_v] = strtok(new_v,'.');
+		
+		% Need to be numbers
+		new_info.major = str2num(new_info.major);
+		new_info.minor = str2num(new_info.minor);
+		new_info.point = str2num(new_info.point);
+		
+		% Check if it's good
+		is_good = 1;
+		try
+			PTBVersionCheck(version(1), version(2), version(3), mode, new_info, 0);
+		catch
+			is_good = 0;
+		end	
+
+		% If so, print out a message
+		if is_good
+			disp(['Try running the command UpdatePTBWrapper([], ' info.past_versions{i}{2} ').']);
+			disp(' ');
+			error('Bad version');
 		end
-		disp(d_str);
 	end
-	disp(' ');
-	
-	% Error us out
-	error('Bad version.');
+
+	% Otherwise, somethings wrong
+	error('Bad version, and there appear to be no good alternatives. Sorry...');
 end
 
